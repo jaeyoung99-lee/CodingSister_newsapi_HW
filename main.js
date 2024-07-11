258adb9d180065c115ad3ac7d3705f4d7d3e0a44
jaeyoung99-lee <leejy0715@naver.com>
2024-07-11 23:58:26 +0900

    search by category and keyword

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const API_KEY = `fcf420e215274c94ad037440fcbb825f`;
 let newsList = [];
+const menus = document.querySelectorAll(".menus button");
+// console.log("menus: ", menus);
+menus.forEach((menu) =>
+  menu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+const sideMenus = document.querySelectorAll(".side-menu-list button");
+sideMenus.forEach((sideMenu) =>
+  sideMenu.addEventListener("click", (event) => getNewsByCategory(event))
+);
+document
+  .getElementById("search-input")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      getNewsByKeyword();
+    }
+  });
 
 const getLatestNews = async () => {
   // URL : URL 인스턴스 -> url에 필요한 함수와 변수들을 제공함
@@ -16,6 +32,39 @@ const getLatestNews = async () => {
 
   newsList = data.articles;
   console.log("data.articles: ", newsList);
+
+  render();
+};
+
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log("category: ", category); // 카테고리 눌렀을 때 클릭 이벤트 잘 작동하는지 확인
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("data: ", data);
+
+  newsList = data.articles;
+
+  render();
+};
+
+const getNewsByKeyword = async () => {
+  const keyword = document.getElementById("search-input").value;
+  console.log("keyword: ", keyword); // 카테고리 눌렀을 때 클릭 이벤트 잘 작동하는지 확인
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("keyword data: ", data);
+
+  newsList = data.articles;
+
+  document.getElementById("search-input").value = "";
+
   render();
 };
 
